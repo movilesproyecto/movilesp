e0738762536b1c8ad70890f2f955466bf1145255
bbaque07 <bryanbaquemoreira@gmail.com>
2026-01-08 16:25:45 -0500

    Preparacion para consumir APIs Rest

---
+++ src/services/apiClient.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+// IMPORTANTE: agrega /api
+const API_URL = "http://TU_IP_O_DOMINIO/api";
+const apiClient = axios.create({
+  baseURL: API_URL,
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  },
+});
+
+apiClient.interceptors.request.use(async (config) => {
+  const token = await AsyncStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+export default apiClient;
